a144aefe505ca4a277c8fe9b30fc3e63ac0eef9a
Jannik-Hm <jannik.heym@gmx.de>
2026-03-04 23:13:37 +0100

    fixed fixturemodel filtering non addressed fixtures

--- pkg/types/fixture.go
+++ pkg/types/fixture.go
@@ -66,8 +66,8 @@ func (a *Fixture) ReadMesh(fileMap map[string]*zip.File) error {
 func (a *Fixture) addNodeModelsToStageModel(stageModel *StageModel, modelConfig ModelConfig, parentConfig ModelNodeConfig) {
 	config := getConfigOverrides(modelConfig, parentConfig, a.UUID)
 
-	if config.Exclude == nil ||
-		!(*config.Exclude) ||
+	if (config.Exclude == nil ||
+		!(*config.Exclude)) &&
 		!(config.RenderOnlyAddressedFixture != nil && *config.RenderOnlyAddressedFixture &&
 			(a.Addresses == nil || len(a.Addresses.Addresses) == 0)) { // remove unpatched fixtures if desired
 		stageModel.FixtureModels = append(stageModel.FixtureModels, a.Model)
